241d1ac3bd78ca33eb7d5526efa281889e32fe96
masonshi1 <33339488+masonshi1@users.noreply.github.com>
2017-12-10 12:08:33 -0500

    Add files via upload

---
+++ hello_aliens.R
@@ -0,0 +1,9 @@
+cat("
+Dear aliens, 
+    
+  My name is Mason. I have a cat and two dogs. If you don't know what those are that's okay. If
+    you don't speak English that is also okay. But then that would be a problem because you can't read this. Anyway, 
+    I hope you guys are peaceful and don't try to kill us. However, I cannot guarantee that we will not try to kill you.
+    
+    Sincerely, 
+    Mason")
